d7fabf4c1e4520050441a9af8789384bfe1363c7
Mustafa Filiz <mustafafiliz18@gmail.com>
2021-11-23 08:48:05 +0300

    first commit

---
+++ src/utils/data.js
@@ -0,0 +1,146 @@
+export const data = [
+	{
+		id: 1,
+		title: "Big Mac",
+		price: 2,
+		img: "https://neal.fun/spend/images/big-mac.jpg"
+	},
+	{
+		id: 2,
+		title: "Flip Flops",
+		price: 3,
+		img: "https://neal.fun/spend/images/flip-flops.jpg"
+	},
+	{
+		id: 3,
+		title: "Coca-Cola Pack",
+		price: 5,
+		img: "https://neal.fun/spend/images/coca-cola-pack.jpg"
+	},
+	{
+		id: 4,
+		title: "Movie Ticket",
+		price: 12,
+		img: "https://neal.fun/spend/images/movie-ticket.jpg"
+	},
+	{
+		id: 5,
+		title: "Book",
+		price: 15,
+		img: "https://neal.fun/spend/images/book.jpg"
+	},
+	{
+		id: 6,
+		title: "Lobster Dinner",
+		price: 45,
+		img: "https://neal.fun/spend/images/lobster-dinner.jpg"
+	},
+	{
+		id: 7,
+		title: "Year of Netflix",
+		price: 100,
+		img: "https://neal.fun/spend/images/year-of-netflix.jpg"
+	},
+	{
+		id: 8,
+		title: "Airpods",
+		price: 199,
+		img: "https://neal.fun/spend/images/airpods.jpg"
+	},
+	{
+		id: 9,
+		title: "Gaming Console",
+		price: 299,
+		img: "https://neal.fun/spend/images/gaming-console.jpg"
+	},
+	{
+		id: 10,
+		title: "Smartphone",
+		price: 699,
+		img: "https://neal.fun/spend/images/smartphone.jpg"
+	},
+	{
+		id: 11,
+		title: "Diamond Ring",
+		price: 10000,
+		img: "https://neal.fun/spend/images/diamond-ring.jpg"
+	},
+	{
+		id: 12,
+		title: "Rolex",
+		price: 15000,
+		img: "https://neal.fun/spend/images/rolex.jpg"
+	},
+	{
+		id: 13,
+		title: "Tesla",
+		price: 750000,
+		img: "https://neal.fun/spend/images/tesla.jpg"
+	},
+	{
+		id: 14,
+		title: "Ferrari",
+		price: 250000,
+		img: "https://neal.fun/spend/images/ferrari.jpg"
+	},
+	{
+		id: 15,
+		title: "House",
+		price: 300000,
+		img: "https://neal.fun/spend/images/single-family-home.jpg"
+	},
+	{
+		id: 16,
+		title: "Gold Bar",
+		price: 700000,
+		img: "https://neal.fun/spend/images/gold-bar.jpg"
+	},
+	{
+		id: 17,
+		title: "Yacht",
+		price: 7500000,
+		img: "https://neal.fun/spend/images/yacht.jpg"
+	},
+	{
+		id: 18,
+		title: "Formula 1 Car",
+		price: 15000000,
+		img: "https://neal.fun/spend/images/formula-1-car.jpg"
+	},
+	{
+		id: 19,
+		title: "Mansion",
+		price: 45000000,
+		img: "https://neal.fun/spend/images/mansion.jpg"
+	},
+	{
+		id: 20,
+		title: "Boeing 747",
+		price: 148000000,
+		img: "https://neal.fun/spend/images/boeing-747.jpg"
+	},
+	{
+		id: 21,
+		title: "Mona Lisa",
+		price: 780000000,
+		img: "https://neal.fun/spend/images/mona-lisa.jpg"
+	},
+	{
+		id: 22,
+		title: "Skyscraper",
+		price: 850000000,
+		img: "https://neal.fun/spend/images/skyscraper.jpg"
+	},
+	{
+		id: 23,
+		title: "Cruise Ship",
+		price: 930000000,
+		img: "https://neal.fun/spend/images/cruise-ship.jpg"
+	},
+	{
+		id: 24,
+		title: "NBA Team",
+		price: 2120000000,
+		img: "https://neal.fun/spend/images/nba-team.jpg"
+	},
+]
